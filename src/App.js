b4296fa286bc59b004383814bfbfd5b9388f2ddf
cfedwinwang <cfedwinwwang@gmail.com>
2021-03-01 01:39:52 +0800

    setting finish

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import "./css/App.css";
+
+export default function APP() {
+    return(
+      <div className="App">
+        <h1> Hello, World! </h1>
+      </div>
+    );
+}
